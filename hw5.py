e07b7c84dd8151ad362d1964847b95ec742ed77c
Luming Wu <reallumingwu@gmail.com>
2016-03-10 20:12:35 -0500

    Things work!

--- hw5.py
+++ hw5.py
@@ -75,51 +75,60 @@ class StringLiteral(Node):
     def evaluate(self):
         return self.value
 
-class Multiply(Node):
+class And(Node):
     """
-    A node representing multiplication.
+    A node representing and.
     """
 
     def __init__(self, left, right):
-        print("Operation multiply ", left.evaluate(), right.evaluate());
+        print("Operation and ", left.evaluate(), right.evaluate());
         self.left = left
         self.right = right
 
     def evaluate(self):
         left = self.left.evaluate()
         right = self.right.evaluate()
-        if isinstance(left, int):
-            if not isinstance(right, int):
-                raise SemanticError()
-        elif isinstance(left, float):
-            if not isinstance(right, float):
-                raise SemanticError()
-        else:
+        if not isinstance(left, int) or not isinstance(right, int):
             raise SemanticError()
-        return left * right
+        if left and right:
+            return 1
+        return 0
 
-class Divide(Node):
+class Or(Node):
     """
-    A node representing division.
+    A node representing or.
     """
 
     def __init__(self, left, right):
-        print("Operation divide ", left.evaluate(), right.evaluate());
+        print("Operation or ", left.evaluate(), right.evaluate());
         self.left = left
         self.right = right
 
     def evaluate(self):
         left = self.left.evaluate()
         right = self.right.evaluate()
-        if isinstance(left, int):
-            if not isinstance(right, int) or right is 0:
-                raise SemanticError()
-        elif isinstance(left, float):
-            if not isinstance(right, float) or right is 0.0:
-                raise SemanticError()
-        else:
+        if not isinstance(left, int) or not isinstance(right, int):
             raise SemanticError()
-        return left / right
+        if left or right:
+            return 1
+        return 0
+
+class Not(Node):
+    """
+    A node representing not.
+    """
+
+    def __init__(self, left):
+        print("Operation not ", left.evaluate());
+        self.left = left
+
+    def evaluate(self):
+        left = self.left.evaluate()
+        if not isinstance(left, int):
+            raise SemanticError()
+        if left:
+            return 0
+        return 1
 
 class Add(Node):
     """
@@ -167,61 +176,94 @@ class Subtract(Node):
             raise SemantcError()
         return left - right
 
-class Not(Node):
+class Multiply(Node):
     """
-    A node representing not.
-    """
-
-    def __init__(self, left):
-        print("Operation not ", left.evaluate());
-        self.left = left
-
-    def evaluate(self):
-        left = self.left.evaluate()
-        if not isinstance(left, int):
-            raise SemanticError()
-        if left:
-            return 0
-        return 1
-
-class Or(Node):
-    """
-    A node representing or.
+    A node representing multiplication.
     """
 
     def __init__(self, left, right):
-        print("Operation or ", left.evaluate(), right.evaluate());
+        print("Operation multiply ", left.evaluate(), right.evaluate());
         self.left = left
         self.right = right
 
     def evaluate(self):
         left = self.left.evaluate()
         right = self.right.evaluate()
-        if not isinstance(left, int) or not isinstance(right, int):
+        if isinstance(left, int):
+            if not isinstance(right, int):
+                raise SemanticError()
+        elif isinstance(left, float):
+            if not isinstance(right, float):
+                raise SemanticError()
+        else:
             raise SemanticError()
-        if left or right:
-            return 1
-        return 0
+        return left * right
 
-class And(Node):
+class Divide(Node):
     """
-    A node representing and.
+    A node representing division.
     """
 
     def __init__(self, left, right):
-        print("Operation and ", left.evaluate(), right.evaluate());
+        print("Operation divide ", left.evaluate(), right.evaluate());
         self.left = left
         self.right = right
 
     def evaluate(self):
         left = self.left.evaluate()
         right = self.right.evaluate()
-        if not isinstance(left, int) or not isinstance(right, int):
+        if isinstance(left, int):
+            if not isinstance(right, int) or right is 0:
+                raise SemanticError()
+        elif isinstance(left, float):
+            if not isinstance(right, float) or right is 0.0:
+                raise SemanticError()
+        else:
             raise SemanticError()
-        if left and right:
-            return 1
-        return 0
+        return left / right
 
+class FloorDivide(Node):
+    """
+    A node representing floor divide.
+    """
+
+    def __init__(self ,left, right):
+        print("Operation floor divide ", left.evaluate(), right.evaluate())
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        left = self.left.evaluate()
+        right = self.right.evaluate()
+        if isinstance(left, int):
+            if not isinstance(right, int) or right is 0:
+                raise SemanticError()
+        elif isinstance(left, float):
+            if not isinstance(right, float) or right is 0.0:
+                raise SemanticError()
+        else:
+            raise SemanticError()
+        return left // right
+
+class Power(Node):
+    """
+    A node representing power.
+    """
+
+    def __init__(self, left, right):
+        print("Operation power ", left.evaluate(), right.evaluate())
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        left = self.left.evaluate()
+        right = self.right.evaluate()
+        if not isinstance(left, int) or not isinstance(left, float):
+            raise SemanticError()
+        if not isinstance(right, int) or not isinstance(right, float):
+            raise SemanticError()
+        return left ** right
+    
 # This is the TPG Parser that is responsible for turning our language into
 # an abstract syntax tree.
 class Parser(tpg.Parser):
@@ -234,29 +276,48 @@ class Parser(tpg.Parser):
     
     START/a -> Expression/a;
 
-    Expression/a -> Number/a | Boolean/a | string/a;
+    Expression/a -> Compare/a;
+
+    Compare/a -> Equals/a;
+
+    Equals/a -> Arrows/a ("==" Arrows/b $ a = Equal(a, b)$
+    | "!=" Arrows/b $ a = NotEqual(a, b)$)*;
+
+    Arrows/a -> CompareFact/a ("<" CompareFact/b $ a = Less(a, b)$
+    | "<=" CompareFact/b $ a = LessEqual(a, b)$
+    | ">" CompareFact/b $ a = Larger(a, b)$
+    | ">=" CompareFact/b $ a = LargerEqual(a, b)$)*;
+
+    CompareFact/a -> CompareLiteral/a | "\(" Compare/a "\)";
+
+    CompareLiteral/a -> Boolean/a | string/a;
+
+    Boolean/a -> BooleanAnd/a;
+    
+    BooleanAnd/a -> BooleanOr/a ("AND" BooleanOr/b $ a = And(a, b)$)*;
+    
+    BooleanOr/a -> BooleanNot/a ("OR" BooleanNot/b $ a = Or(a, b)$)*;
+    
+    BooleanNot/a -> BooleanFact/a | "NOT" BooleanFact/a $ a = Not(a)$;
+
+    BooleanFact/a -> BooleanLiteral/a | "\(" Boolean/a "\)";
+
+    BooleanLiteral/a -> boolean/a | Number/a;
     
     Number/a -> Addsub/a;
 
     Addsub/a -> Muldiv/a("\+" Muldiv/b $ a = Add(a, b)$
     | "-" Muldiv/b $ a = Subtract(a, b)$)*;
     
-    Muldiv/a -> NumberFact/a("\*" NumberFact/b $ a = Multiply(a, b)$
-    | "/" NumberFact/b $ a = Divide(a, b) $)*;
+    Muldiv/a -> Pow/a("\*" Pow/b $ a = Multiply(a, b)$
+    | "/" Pow/b $ a = Divide(a, b) $
+    | "//" Pow/b $ a = FloorDivide(a, b)$)*;
+
+    Pow/a -> NumberFact/a ("\*\*" NumberFact/b $ a = Power(a, b)$)*;
     
     NumberFact/a -> NumLiteral/a | "\(" Number/a "\)"; 
 
     NumLiteral/a -> int/a | real/a;
-
-    Boolean/a -> BooleanAnd/a;
-    
-    BooleanAnd/a -> BooleanOr/a ("AND" BooleanOr/b $ a = And(a, b)$)*;
-    
-    BooleanOr/a -> int/a ("OR" int/b $ a = Or(a, b)$)*;
-
-    BooleanNot/a -> BooleanLiteral/a | "NOT" BooleanLiteral/a $ a = Not(a)$;
-
-    BooleanLiteral/a -> boolean/a | int/a;
     """
 
 # Make an instance of the parser. This acts like a function.
